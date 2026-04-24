6fd8644b84ef2f50d3a2b9776c335b83fa5b9f55
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-04-24 15:12:16 +0800

    the space is removed

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function remove(input){
     let array = input.split('');
-
-    return array;
+    return array.reduce(function(accumulator, item){
+        if(item !== ' '){
+            accumulator.push(item);
+        }
+        return accumulator;
+    }, []);
+   
 }
 
 console.log(remove('A car, a man, a maraca.'));
